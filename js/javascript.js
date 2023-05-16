a85fba51fa970ca82f0715c46c7217aef6783662
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-05-16 17:18:51 +0500

    Setup JS Linters
    Fix JS Linters errors

--- js/javascript.js
+++ js/javascript.js
@@ -1,32 +1,30 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active")
-    navMenu.classList.toggle("active")
-    document.body.classList.toggle('no-scroll');
-})
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  document.body.classList.toggle('no-scroll');
+});
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-    document.body.classList.remove('no-scroll');
-}))
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+  document.body.classList.remove('no-scroll');
+}));
 
 // Get the header
-var header = document.getElementById("myheader");
+const header = document.getElementById('myheader');
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+const sticky = header.offsetTop;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// Add the sticky class to the header Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
+    header.classList.add('sticky');
   } else {
-    header.classList.remove("sticky");
+    header.classList.remove('sticky');
   }
 }
+window.onscroll = myFunction();
